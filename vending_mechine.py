b158464bc806da6d22214328b4cc3850ac36fe7d
Abdul Rahman bin Ibrahim <rahman@xoxzo.com>
2024-04-03 19:02:32 +0800

    Add files via upload

---
+++ vending_mechine.py
@@ -0,0 +1,22 @@
+def vending_machine(drink, cash):
+    notes = [100, 50, 20, 10, 5, 1]
+    drink = drink.title()
+    drinks = {
+        "Coke": 1,
+        "Pepsi": 2,
+        "Wonda": 5,
+        "Evian": 9,
+    }
+    note_return = {}
+    balance = cash - drinks[drink]
+    for x in notes:
+    	divisible = balance - balance % x
+    	balance = balance % x
+    	note_return["RM"+str(x)] = int(divisible / x) 
+    	    
+    return (note_return)
+    
+cash = int(input("I have RM"))
+drink = input("I wish to buy <Coke(RM1)/Pepsi(RM2)/Wonda(RM5)/Evian(RM9)>:")
+print("Returned least number of notes is: "+str((vending_machine(drink, cash)))) 
+
