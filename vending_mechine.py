98877629fcc426eb6cda166cb4a733b35caa36bc
Abdul Rahman bin Ibrahim <rahman@xoxzo.com>
2024-04-04 08:46:21 +0800

    Bring dict out to make function more plug and play

--- vending_mechine.py
+++ vending_mechine.py
@@ -1,12 +1,6 @@
-def vending_machine(drink, cash):
+def vending_machine(drinks, drink, cash):
     notes = [100, 50, 20, 10, 5, 1]
     drink = drink.title()
-    drinks = {
-        "Coke": 1,
-        "Pepsi": 2,
-        "Wonda": 5,
-        "Evian": 9,
-    }
     note_return = {}
     balance = cash - drinks[drink]
     for x in notes:
@@ -15,8 +9,14 @@ def vending_machine(drink, cash):
     	note_return["RM"+str(x)] = int(divisible / x) 
     	    
     return (note_return)
-    
+
+drinks = {
+    "Coke": 1,
+    "Pepsi": 2,
+    "Wonda": 5,
+    "Evian": 9,
+}
 cash = int(input("I have RM"))
 drink = input("I wish to buy <Coke(RM1)/Pepsi(RM2)/Wonda(RM5)/Evian(RM9)>:")
-print("Returned least number of notes is: "+str((vending_machine(drink, cash)))) 
+print("Returned least number of notes is: "+str((vending_machine(drinks, drink, cash)))) 
 
